53eace0acd6d1de11b5f6a7be0d066c175d7d73f
Alex Sharpe <asharpe@Alexs-MacBook-Pro.local>
2022-11-11 09:58:18 -0500

    Initial commit

---
+++ app/src/main/java/com/xxalexdevxx/androidrxjava/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xxalexdevxx.androidrxjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
